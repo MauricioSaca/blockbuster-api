ed3d07dd6441f7564688519e4d69a4282277adf9
msaca <saca.menendez@gmail.com>
2019-12-14 19:02:00 -0600

    user likes movies

---
+++ src/main/java/com/blockbuster/api/controllers/LikesMovieController.java
@@ -0,0 +1,53 @@
+package com.blockbuster.api.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.blockbuster.api.models.Movie;
+import com.blockbuster.api.models.MoviesLikes;
+import com.blockbuster.api.models.UserPrincipal;
+import com.blockbuster.api.pojos.ApiResponse;
+import com.blockbuster.api.repository.MovieRepository;
+import com.blockbuster.api.repository.MoviesLikesRepository;
+import com.blockbuster.api.utils.SecurityUtils;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@RestController
+@RequestMapping("/blockbuster")
+@Validated
+@Getter
+@Setter
+public class LikesMovieController {
+
+	@Autowired
+	private MoviesLikesRepository moviesLikesRepository;
+
+	@Autowired
+	private MovieRepository movieRepository;
+
+	@PostMapping("/like/movies/{id}/userlike")
+	public ResponseEntity<?> likeUserMovie(@PathVariable("id") Long id) {
+
+		if (movieRepository.existsById(id)) {
+			UserPrincipal user = SecurityUtils.getCurrentUser();
+			Movie movie = movieRepository.findById(id).orElse(new Movie());
+			MoviesLikes likeMovie = new MoviesLikes();
+			likeMovie.setUserPrincipal(user);
+			likeMovie.setMovie(movie);
+			moviesLikesRepository.save(likeMovie);
+		} else {
+			return new ResponseEntity(new ApiResponse(false, "Movie not exist!"), HttpStatus.BAD_REQUEST);
+		}
+
+		return ResponseEntity.ok(new ApiResponse(true, "like registered"));
+	}
+
+}
