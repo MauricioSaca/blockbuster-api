02b7fe72fd6f0219c5d0357c5f280a7b44a981c2
msaca <saca.menendez@gmail.com>
2019-12-14 23:47:39 -0600

    disabled create database

--- src/main/java/com/blockbuster/api/config/BlockBusterApplication.java
+++ src/main/java/com/blockbuster/api/config/BlockBusterApplication.java
@@ -1,24 +1,12 @@
 package com.blockbuster.api.config;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-import com.blockbuster.api.enums.RoleEnum;
-import com.blockbuster.api.models.Authorities;
-import com.blockbuster.api.models.UserPrincipal;
-import com.blockbuster.api.repository.AuthoritiesRepository;
-import com.blockbuster.api.repository.UserPrincipalRepository;
 
 @SpringBootApplication
 @ComponentScan({ "com.blockbuster.api*" })
@@ -33,51 +21,6 @@ public class BlockBusterApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlockBusterApplication.class, args);
-	}
-
-	@Bean
-	CommandLineRunner initDatabase(UserPrincipalRepository userRepository,
-			AuthoritiesRepository authoritiesRepository) {
-
-		List<UserPrincipal> defaultUserList = new ArrayList<>();
-		List<Authorities> defaultAuthoritiesList = new ArrayList<>();
-
-		UserPrincipal adminUser = new UserPrincipal();
-		String password = new BCryptPasswordEncoder().encode("admin");
-		adminUser.setName("Mauricio");
-		adminUser.setLastName("Saca");
-		adminUser.setUsername("msaca");
-		adminUser.setPassword(password);
-		adminUser.setEmail("saca.menendez@gmai.com");
-		adminUser.setEnabled(true);
-
-		Authorities auth = new Authorities();
-		auth.setUserPrincipal(adminUser);
-		auth.setRole(RoleEnum.ADMIN);
-
-		UserPrincipal normalUser = new UserPrincipal();
-		String passwordUser = new BCryptPasswordEncoder().encode("user");
-		normalUser.setName("Applaudo");
-		normalUser.setLastName("Studio");
-		normalUser.setUsername("astudio");
-		normalUser.setPassword(passwordUser);
-		normalUser.setEmail("saca.menendez@gmai.com");
-		normalUser.setEnabled(true);
-
-		Authorities authUser = new Authorities();
-		authUser.setUserPrincipal(normalUser);
-		authUser.setRole(RoleEnum.USER);
-		
-		defaultUserList.add(adminUser);
-		defaultUserList.add(normalUser);
-		
-		defaultAuthoritiesList.add(auth);
-		defaultAuthoritiesList.add(authUser);
-
-		return args -> {
-			userRepository.saveAll(defaultUserList);
-			authoritiesRepository.saveAll(defaultAuthoritiesList);
-		};
 	}
 
 }
